af3e76f179d080b85150d101c8ee1b6fa111092b
Xiaojun928 <yoyostudents5775@gmail.com>
2020-06-12 14:05:46 +0800

    Update s1.make_mauve_align.sh

--- recombination_inference/s1.make_mauve_align.sh
+++ recombination_inference/s1.make_mauve_align.sh
@@ -4,11 +4,9 @@
 
 cat <<EOF > mauve_align.sh
 #!/bin/bash
-APP_NAME=FN_small
-RUN="RAW"
-NP=4
 
-/home-fn/users/nscc1082/software/mauve_2.3.1/linux-x64/progressiveMauve --output=mauve_out.xmfa \\
+
+/yourpath/mauve_2.3.1/linux-x64/progressiveMauve --output=mauve_out.xmfa \\
 EOF
 
 find genomic_fasta/*.fna | sed ':a;N;$!ba;s/\n/ \\\n/g' >> mauve_align.sh
