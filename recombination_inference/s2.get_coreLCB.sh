3024e27cfb06fc6c714a2a3ae356a94d78d02ca7
Xiaojun928 <yoyostudents5775@gmail.com>
2020-06-12 14:06:25 +0800

    Update s2.get_coreLCB.sh

--- recombination_inference/s2.get_coreLCB.sh
+++ recombination_inference/s2.get_coreLCB.sh
@@ -6,11 +6,9 @@
 #      so the input xfma file should be located at the same path like the directory is. 
 #      Otherwise, Exception FileNotOpened thrown from Unknown()  in gnFileSource.cpp 67
 
-##for cluster in the lab
-#/home-user/software/mauve/mauve_snapshot_2015-02-13/stripSubsetLCBs roseo_v1.xmfa roseo_v1.xmfa.bbcols test.xmfa 500 16
 
 ## for cluster in ShenZhen
-/home-fn/users/nscc1082/software/mauve_snapshot_2015-02-13/stripSubsetLCBs mauve_out.xmfa mauve_out.xmfa.bbcols coreLCB.xmfa 500 16
+/yourpath/mauve_snapshot_2015-02-13/stripSubsetLCBs mauve_out.xmfa mauve_out.xmfa.bbcols coreLCB.xmfa 500 16
 
 #Usage: stripSubsetLCBs <input xmfa> <input bbcols> <output xmfa> [min LCB size] [min genomes] [randomly subsample to X kb]
 
